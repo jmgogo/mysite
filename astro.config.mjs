36671c40a9af6483f029cc3a0ba0f574a9890365
Jose M. Gomez <gomezjosem@live.com>
2026-01-05 22:02:12 +0000

    refactor: use experimental fonts api with automatic web font optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,4 +16,65 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  experimental: {
+    fonts: [
+      {
+        fallbacks: ["serif", "ui-serif"],
+        provider: "local",
+        name: "Compagnon",
+        cssVariable: "--font-compagnon",
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/Compagnon/Compagnon-Roman.woff2",
+              "./src/assets/fonts/Compagnon/Compagnon-Roman.woff",
+            ],
+            weight: 400,
+            style: "normal",
+            display: "swap",
+          },
+
+          {
+            src: [
+              "./src/assets/fonts/Compagnon/Compagnon-Italic.woff2",
+              "./src/assets/fonts/Compagnon/Compagnon-Italic.woff",
+            ],
+            weight: 300,
+            style: "italic",
+            display: "swap",
+          },
+
+          {
+            src: [
+              "./src/assets/fonts/Compagnon/Compagnon-Light.woff2",
+              "./src/assets/fonts/Compagnon/Compagnon-Light.woff",
+            ],
+            weight: 300,
+            style: "normal",
+            display: "swap",
+          },
+
+          {
+            src: [
+              "./src/assets/fonts/Compagnon/Compagnon-Bold.woff2",
+              "./src/assets/fonts/Compagnon/Compagnon-Bold.woff",
+            ],
+            weight: 700,
+            style: "italic",
+            display: "swap",
+          },
+
+          {
+            src: [
+              "./src/assets/fonts/Compagnon/Compagnon-Medium.woff2",
+              "./src/assets/fonts/Compagnon/Compagnon-Medium.woff",
+            ],
+            weight: 700,
+            style: "normal",
+            display: "swap",
+          },
+        ],
+      },
+    ],
+  },
 });
