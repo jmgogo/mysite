f097ae3623974032b0af36214da1908c8f5e9bc4
Jose M. Gomez <gomezjosem@live.com>
2026-01-07 02:20:23 +0000

    fix: update site root url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://jmgogo.github.io/mysite/",
   integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
